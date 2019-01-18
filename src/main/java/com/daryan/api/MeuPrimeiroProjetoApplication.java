2ff67e253d67853f26101391f007e5434f2fd913
Daryan <daryan.spfc@gmail.com>
2019-01-17 23:40:21 -0200

    Alteração no arquivo principal da aplicação

--- src/main/java/com/daryan/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/daryan/api/MeuPrimeiroProjetoApplication.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		
+		System.out.println("Meu primeiro projeto!");
 	}
-
 }
-
